d81ec1a7eb7e20057543ad7995c4db86b10ef2c1
draos2q <draos2q.caw@proton.me>
2023-04-09 21:51:43 -0600

    fix : language code

--- src/utilities/languages.ts
+++ src/utilities/languages.ts
@@ -4,7 +4,7 @@ const languages: LanguageModel[] = [
     { "code": "en", "name": "English", "nativeName": "English", rtl: false },
     { "code": "de", "name": "German", "nativeName": "Deutsch", rtl: false },
     { "code": "fr", "name": "French", "nativeName": "Français, langue française", rtl: false },
-    { "code": "fr", "name": "Spanish", "nativeName": "Español, castellano", rtl: false },
+    { "code": "es", "name": "Spanish", "nativeName": "Español, castellano", rtl: false },
     { "code": "ja", "name": "Japanese", "nativeName": "日本語 (にほんご／にっぽんご)", rtl: false },
     { "code": "zh", "name": "Chinese", "nativeName": "中文 (Zhōngwén), 汉语, 漢語", rtl: false },
     { "code": "tr", "name": "Turkish", "nativeName": "Türkçe", rtl: false },
