15a6636c12f61ca34201c9cfe28e432231c85f34
draos2q <draos2q.caw@proton.me>
2023-05-03 02:15:07 -0600

    feat: fetch contributors from an easy to update repo

---
+++ src/services/Contributors.ts
@@ -0,0 +1,30 @@
+
+import { Contributor } from "src/types";
+
+export const fetchContributors = async (): Promise<Contributor[]> => {
+
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/draos2q/caw-contributors/main/contributors.json?token=GHSAT0AAAAAACCE5U5EXO3ISZ62ETKAZA4WZCSCTLQ');
+        const result = await response.json();
+
+        if (Array.isArray(result)) {
+
+            return result.map((c) => {
+
+                const _contributor: Contributor = {
+                    id: c.id || '',
+                    avatar: c.avatar || '',
+                    name: c.name || '',
+                    profile: c.profile || '',
+                };
+
+                return _contributor;
+            })
+        }
+
+        return [];
+    }
+    catch (error) {
+        return [];
+    }
+}
