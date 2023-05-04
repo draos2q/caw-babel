efd8e7bf44986295cc377ee4d3f5438c4a7296dd
draos2q <draos2q.caw@proton.me>
2023-05-03 23:37:57 -0600

    Updates

---
+++ assets/Page404-d22fb5ab.js
@@ -0,0 +1,1 @@
+import{j as e,F as r,a,B as n}from"./index-77d4b674.js";function s(){return e(r,{children:[a("h1",{children:"404 - Page Not Found"}),a(n,{as:"a",href:"/",children:"Go Home"})]})}export{s as default};
---
+++ assets/chunk-P74GIWPW-61625518.js
@@ -0,0 +1,1 @@
+import{g as n,p as i,o,a9 as r,a as x,i as l,e as m}from"./index-77d4b674.js";var g=n(function(a,t){const s=i("Text",a),{className:f,align:d,decoration:N,casing:h,...e}=o(a),c=r({textAlign:a.align,textDecoration:a.decoration,textTransform:a.casing});return x(l.p,{ref:t,className:m("chakra-text",a.className),...c,...e,__css:s})});g.displayName="Text";export{g as T};
